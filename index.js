eee8e64f96fa3577b019068d139dfe7cc8a8c657
tylergreason <tyler.greason@protonmail.com>
2020-02-20 16:52:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+const documentBody = document.querySelector("body"); 
+
+function submitData(name, email){
+    let objectData ={
+        name: name, 
+        email: email
+    }
+
+    let configObj = {
+        method: "POST", 
+        headers: {
+            "Content-type": "application/json", 
+            "Accept": "application/json"
+        }, 
+        body: JSON.stringify(objectData)
+    }; 
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response){
+        return response.json(); 
+    })
+    .then(function(object){
+        console.log(object)
+        // debugger;
+        const idListing = document.createElement('p'); 
+        idListing.innerHTML = object.id; 
+        documentBody.appendChild(idListing)
+    })
+
+        .catch(function(error){
+            let errorPara = document.createElement('p');
+            errorPara.innerHTML = error.message; 
+            documentBody.appendChild(errorPara);
+        }
+    )
+
+}
